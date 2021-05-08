23744a9136bd43e0b6fabacec4f7dfdffb47c6f3
Borna Gojšić <69301624+bornagojsic@users.noreply.github.com>
2021-05-08 17:48:25 +0200

    Removing unnecessary files

--- dek.py
+++
@@ -1,9 +0,0 @@
-from random import shuffle as promijesaj
-
-dek = '''
-		7♠ 8♠ 9♠ 10♠ B♠ D♠ K♠ A♠
-		7♥ 8♥ 9♥ 10♥ B♥ D♥ K♥ A♥
-		7♦ 8♦ 9♦ 10♦ B♦ D♦ K♦ A♦
-		7♣ 8♣ 9♣ 10♣ B♣ D♣ K♣ A♣
-		'''.split()
-
